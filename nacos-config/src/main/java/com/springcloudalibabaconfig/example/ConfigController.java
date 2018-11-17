178ff40c26c4f87f6b1c579e29f5fc8958b62be4
zjx <7532554@gmail.com>
2018-11-17 21:24:32 +0800

    config

--- nacos-config/src/main/java/com/springcloudalibabaconfig/example/ConfigController.java
+++ nacos-config/src/main/java/com/springcloudalibabaconfig/example/ConfigController.java
@@ -4,15 +4,27 @@ package com.springcloudalibabaconfig.example;
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.client.config.NacosConfigService;
+import org.bouncycastle.crypto.tls.SRTPProtectionProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RefreshScope
 public class ConfigController {
 
+
+    /**
+    * nacos-config's dataId = ${prefix}-${spring.profile.active}.${file-extentions}
+     * ${prefix}=spring.cluod.nacos.config.prefix,default is the applicationName
+     * ${spring.profile.prefix}
+     * ${file-extentions}=properties or yaml
+    */
+    @Value("${spring.application.name}")
+    private String applicaionName;
 
     @Value("${service.discovery}")
     private String serviceUrl;
@@ -32,7 +44,7 @@ public class ConfigController {
     @RequestMapping(value = "/config/update")
     public String updateCOnfig(String configKey,String configValue){
         try {
-            nacosConfigService.publishConfig("","",configKey + "=" + configValue);
+            nacosConfigService.publishConfig(applicaionName+".yaml","DEFAULT_GROUP",configKey + "=" + configValue);
         } catch (NacosException e) {
             e.printStackTrace();
         }
